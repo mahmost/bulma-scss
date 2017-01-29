c9cdb031b2495ed2b575439786736b0c94c26b23
Mahmoud Mostafa <mah@moud.info>
2017-01-30 00:05:30 +0200

    Fix for bulma version 3

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 VERSION=${1:-master} # e.g. 0.0.18
-rm -rf $DIR/../bulma
+#rm -rf $DIR/../bulma
 cd $DIR/..
-git clone https://github.com/jgthms/bulma.git
+#git clone https://github.com/jgthms/bulma.git
 cd $DIR/../bulma
+#git reset --hard
+git checkout master
+git pull
 git fetch origin --tags
 git checkout $VERSION
 find . -name '*.sass' -exec sass-convert -F sass -T scss '{}' '{}.temp' \;
-find . -name '*.sass.temp' -exec rename 's/^(.+)\/(.+?).sass.temp$/$1\/_$2.scss/' '{}' \;
-find . -name '*.sass' -type f -delete
+find . -name '*.sass.temp' -exec sh -c 'x="{}"; dir=$(dirname $x) name="$(basename $x)"; name=${name/.sass.temp/}; mv "$x" "$dir/_$name.scss"' \;
 cd $DIR/..
 rm -rf dist
 mkdir -p dist
-mv bulma/sass dist
-mv bulma/_bulma.scss dist
+cp -a bulma/sass dist
+cp bulma/_bulma.scss dist
+sed -i -e "s/_all/__all/g" dist/_bulma.scss
+find dist/ -name *.scss | xargs sed -i -e "s/\.sass//g"
 cd $DIR/../bulma
-git reset --hard
+# git reset --hard
+find $DIR/../dist -name '*.sass' -type f -delete
+find $DIR/../dist -name '*.temp.scss' -type f -delete
+find . -name '*.scss' -type f -delete
